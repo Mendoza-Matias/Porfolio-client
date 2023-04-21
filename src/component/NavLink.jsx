98960aab1609624a127d0e04af6e070e9d5bb97f
mendoza matias <mendoza.matias.ezequiel.01@gmail.com>
2023-04-21 20:20:41 -0300

    Nuevo cv cargado

--- src/component/NavLink.jsx
+++ src/component/NavLink.jsx
@@ -7,7 +7,7 @@ const NavLink = () =>{
             <li ><Link to={'/'}>Inicio</Link></li>
             <li><Link to={'/skills'}>Mis Skills</Link></li>
             <li><Link to={'/contact'}>Contacto</Link></li>
-            <li><a href="./cv.pdf" download={""}>Cv</a></li>
+            <li><a href="./cv_mendoza_matias.pdf" download={""}>Cv</a></li>
         </>
     )
 }
